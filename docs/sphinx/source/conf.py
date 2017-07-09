0accd2e954da2478c4c132c7f34159477ae4a6e4
cbjones <cbjones@sandia.gov>
2017-07-09 07:54:16 -0600

    update conf.py

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('/Users/cbjones/Documents/ML Code/ANN/ART/art
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'sphinx.ext.autodoc'
+	'sphinx.ext.autodoc',
 	'sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
