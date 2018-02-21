419e10e601475b039a51d054a48223a1a9991105
cbjones <cbjones@sandia.gov>
2018-02-21 07:24:05 -0700

    remove numpy 1.10.4 requirement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from distutils.core import Extension
 
 DISTNAME = 'art-python'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 PACKAGES = ['art']
 EXTENSIONS = []
 DESCRIPTION = 'Python package'
@@ -14,8 +14,7 @@ URL = 'https://github.com/cbirkj/art-python'
 
 setuptools_kwargs = {
     'zip_safe': False,
-    'install_requires': ['numpy >= 1.10.4',
-                         'pandas >= 0.18.0'],
+    'install_requires': ['pandas >= 0.18.0'],
     'scripts': [],
     'include_package_data': True
 }
